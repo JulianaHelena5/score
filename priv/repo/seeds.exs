094cdef4c712bb999e7aaa804116fd2e0bf76f9f
Juliana Helena <juliana.helena15@gmail.com>
2021-07-31 14:53:46 -0300

    Update seeds.exs
    I forgot to add it in the last pr 👎🏼

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+IO.puts("Starting to run seeds file...")
+
+alias Score.Entities.Users
+
+(1..10_000)
+|> Stream.each(fn _ -> Users.create(%{points: 0}) end)
+|> Stream.run()
+
+IO.puts("Successfully runned seeds!...")
