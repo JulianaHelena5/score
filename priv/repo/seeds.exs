8bee049dc88b553836962cadb87dbf500701d065
Juliana Helena <juliana.helena15@gmail.com>
2021-08-02 04:26:29 -0300

    fix(seeds.exs): Improving seeds performance

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,10 +12,34 @@
 
 IO.puts("Starting to run seeds file...")
 
-alias Score.Entities.Users
+alias Score.Entities.Schemas.User
+alias Score.Repo
 
-(1..10_000)
-|> Stream.each(fn _ -> Users.create(%{points: 0}) end)
-|> Stream.run()
+stream =
+  Stream.map(1..1_000_000, fn _ ->
+    # Repo.insert_all() don't insert automatically inserted_at and updated_at
+    date_time =
+      NaiveDateTime.truncate(
+        NaiveDateTime.utc_now(),
+        :second
+      )
+
+    %{points: 0, inserted_at: date_time, updated_at: date_time}
+  end)
+
+Repo.transaction(fn ->
+  stream
+  |> Stream.chunk_every(15_000)
+  |> Task.async_stream(
+    fn batch ->
+      {batches_cursor, _} = Repo.insert_all(User, batch)
+      batches_cursor
+    end,
+    ordered: false
+  )
+  |> Enum.reduce(0, fn {:ok, batches_cursor}, accumulator ->
+    batches_cursor + accumulator
+  end)
+end)
 
 IO.puts("Successfully runned seeds!...")
